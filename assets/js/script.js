dd677174658003708e63699fc41b22fd511eec33
kbogar <krisztianbogar@gmail.com>
2022-12-15 10:38:40 +0000

    Add game state element and declare messages function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// Store a game status element 
+// Store a game status element to easily use it later 
 const gameStatusDisplay = document.querySelector('game-status');
 
 // Declare variables that we will use to track the game state
@@ -8,3 +8,18 @@ let gameActive = true;
 
 // To store current player here, to know whos turn it is
 let currentPlayer = "x";
+
+/**
+ * To store current game state here in form of empty strings in an array will allow us to track
+ * played cells and validate the game state later on
+ */
+let gameState = ["", "", "", "", "", "", "", "", ""];
+
+/**
+ * Declare messages what we will display to the user during the game. Using an arrow function
+ * here a new message gets created with a current player.
+ */
+const winMessage = () => `Player ${currentPlayer} has won!`;
+const drawMessage = () => `It is a draw!`;
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+ 
