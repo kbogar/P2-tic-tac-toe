81a1c4362348ecaccad766e4ec88db0b88e5160a
kbogar <krisztianbogar@gmail.com>
2022-12-15 10:10:01 +0000

    Add game status element and variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
+// Store a game status element 
+const gameStatusDisplay = document.querySelector('game-status');
 
+// Declare variables that we will use to track the game state
+
+// Use in case of an end scenario to pause the game 
+let gameActive = true;
+
+// To store current player here, to know whos turn it is
+let currentPlayer = "x";
